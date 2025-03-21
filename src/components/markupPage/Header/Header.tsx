5e32c124625926fb5f488290a628626b19cf623a
Aleksei Kutukov <kytykob@gmail.com>
2025-03-21 23:06:15 +0300

    add Header

--- src/components/markupPage/Header/Header.tsx
+++ src/components/markupPage/Header/Header.tsx
@@ -1,5 +1,20 @@
+import Search from '@/components/UI/Search/Search';
+import Link from 'next/link';
+import styles from './Header.module.css';
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className={styles.header}>
+      <div className={styles.header_logo_search}>
+        <Link href="/">
+          <div className={styles.logo}>AJIEKCEU K</div>
+        </Link>
+        <div className={styles.search}>
+          <Search />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
