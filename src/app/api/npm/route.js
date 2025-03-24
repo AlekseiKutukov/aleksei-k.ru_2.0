7532c4d0ff9bec09300e3ec74cd2ec7ae1dc2fb5
Aleksei Kutukov <kytykob@gmail.com>
2025-03-24 20:42:13 +0300

    add api react add info in mongodb

--- src/app/api/npm/route.js
+++ src/app/api/npm/route.js
@@ -1,13 +1,18 @@
 import dbConnect from '../../../../lib/mongodb';
 import mongoose from 'mongoose';
 
+//http://localhost:3050/api/npm
+
 export async function GET(req) {
   try {
     await dbConnect();
     const collection = mongoose.connection.db.collection('npm');
     await collection.insertOne({
-      name: 'Алексей',
-      email: 'aleksei@example.com',
+      '1_title': 'query-string',
+      '2_description': '(Router) конвертирует строку запроса в объект js',
+      '3_example': `const parsed = queryString.parse(location.search);
+console.log(parsed);
+//=> {foo: 'bar'}`,
     });
     return new Response(
       JSON.stringify({ message: 'Данные добавлены в MongoDB!' }),
---
+++ src/app/api/react/route.js
@@ -0,0 +1,33 @@
+import dbConnect from '../../../../lib/mongodb';
+import mongoose from 'mongoose';
+
+//http://localhost:3050/api/react
+
+export async function GET(req) {
+  try {
+    await dbConnect();
+    const collection = mongoose.connection.db.collection('react');
+    await collection.insertOne({
+      '1_title': 'useContext',
+      '2_memorandum': 'Позволяет передать данные минуя пропсы.',
+      '3_description':
+        'Этот хук позволяет сохранять переменную или объект, и использовать ее между несколькими компонентами. Под самим же контекстом, понимают эту сохраненную величину.',
+      '4_example': `Этот хук позволяет сохранять переменную или объект, и использовать ее между несколькими компонентами. Под самим же контекстом, понимают эту сохраненную величину.`,
+    });
+    return new Response(
+      JSON.stringify({ message: 'Данные добавлены в MongoDB!' }),
+      {
+        status: 200,
+        headers: { 'Content-Type': 'application/json' },
+      }
+    );
+  } catch (error) {
+    return new Response(
+      JSON.stringify({ error: 'Ошибка подключения', details: error.message }),
+      {
+        status: 500,
+        headers: { 'Content-Type': 'application/json' },
+      }
+    );
+  }
+}
