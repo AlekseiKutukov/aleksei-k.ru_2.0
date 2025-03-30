23ee559f09d03b3677bd23e91dd8f2666fe960c8
Aleksei Kutukov <kytykob@gmail.com>
2025-03-30 08:22:08 +0300

    add page&route from page addArticle

---
+++ src/app/api/addArticle/route.js
@@ -0,0 +1,29 @@
+import dbConnect from '../../../../lib/mongodb';
+
+export async function POST(req) {
+  try {
+    await dbConnect(); // Подключаемся к базе данных
+    const data = await req.json(); // Получаем данные из формы
+
+    const mongoose = require('mongoose');
+    const collection = mongoose.connection.db.collection('react'); // Коллекция 'react'
+
+    await collection.insertOne({
+      '1_title': data['1_title'],
+      '2_memorandum': data['2_memorandum'],
+      '3_description': data['3_description'],
+      '4_example': data['4_example'],
+    });
+
+    return new Response(JSON.stringify({ message: 'Статья добавлена' }), {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  } catch (error) {
+    console.error(error);
+    return new Response(JSON.stringify({ error: 'Ошибка сервера' }), {
+      status: 500,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import Link from 'next/link';
+
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <Link href="/addArticle">
+        <span>add Article</span>
+      </Link>
+    </>
+  );
 }
