f59ad600720bb62274f24ab6f9975b926cda658e
Aleksei Kutukov <kytykob@gmail.com>
2025-04-01 19:33:28 +0300

    add metadata from home

--- src/app/react/page.js
+++ src/app/react/page.js
@@ -1,5 +1,8 @@
 import Link from 'next/link';
+import { notFound } from 'next/navigation';
+import style from './page.module.css';
 
+// Функция для получения списка статей
 async function fetchArticles() {
   const baseUrl = process.env.NEXT_PUBLIC_API_URL;
 
@@ -10,20 +13,52 @@ async function fetchArticles() {
   return res.json();
 }
 
-export default async function ReactPage() {
+// Генерация метаданных (без проверки ошибок)
+export async function generateMetadata() {
   const articles = await fetchArticles();
+  const articleCount = articles.length;
 
-  return (
-    <div>
-      <h1>React Хуки</h1>
+  return {
+    title: 'Всё о React - хуки и примеры кода',
+    description: `Изучайте React с описанием и примерами использования.`,
+  };
+}
 
-      {articles.map((article) => (
-        <div key={article._id}>
-          <Link href={`/react/hooks/${article['1_title']}`}>
-            <h2>{article['1_title']}</h2>
-          </Link>
+// Основная функция страницы
+export default async function ReactPage() {
+  try {
+    const articles = await fetchArticles();
+
+    return (
+      <div>
+        <h1>Все о React - хуки и примеры кода</h1>
+        <h2>Хуки</h2>
+        <div className={style.block_hooks}>
+          {articles.map((article) => (
+            <Link
+              key={article._id}
+              href={`/react/hooks/${article['1_title']}`}
+              className={style.button_hooks}
+            >
+              <span>{article['1_title']}</span>
+            </Link>
+          ))}
         </div>
-      ))}
-    </div>
-  );
+        <h2>Примеры кода</h2>
+        <div className={style.block_hooks}>
+          {articles.map((article) => (
+            <Link
+              key={article._id}
+              href={`/react/hooks/${article['1_title']}`}
+              className={style.button_hooks}
+            >
+              {article['1_title']}
+            </Link>
+          ))}
+        </div>
+      </div>
+    );
+  } catch (error) {
+    notFound(); // При ошибке переходим на 404
+  }
 }
