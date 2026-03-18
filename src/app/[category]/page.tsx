3e8150ddf878bcb351908b03f5a6198d4f47b596
Aleksei_Kutukov <kytykob@gmail.com>
2026-03-18 21:15:28 +0300

    change of routes and portfolio page

--- src/app/[category]/page.tsx
+++ src/app/[category]/page.tsx
@@ -1,6 +1,26 @@
 import { notFound } from "next/navigation";
 import { getArticles } from "@/lib/getArticles";
-import QuestionsList from "./../../components/Questions/QuestionsList";
+import QuestionsList from "@/components/Questions/QuestionsList";
+import ProjectCard from "@/components/ProjectCard/ProjectCard";
+import style from "./page.module.css";
+
+interface IProject {
+  _id: string;
+  title: string;
+  stack: string[];
+  shortDescription: string;
+  links: {
+    github: string;
+    live: string;
+  };
+}
+
+interface IArticle {
+  _id: string;
+  title: string;
+  content: string;
+  category: string;
+}
 
 export default async function CategoryPage({
   params,
@@ -8,17 +28,33 @@ export default async function CategoryPage({
   params: Promise<{ category: string }>;
 }) {
   const { category } = await params;
+  const isPortfolio = category === "portfolio";
 
-  const articles = await getArticles(category);
+  const data = isPortfolio
+    ? await getArticles<IProject>("all", "projects")
+    : await getArticles<IArticle>(category, "articles");
 
-  if (!articles || articles.length === 0) {
-    notFound();
-  }
+  // console.log("Category:", category);
+  // console.log("Data length:", data.length);
+
+  // Проверка на пустоту
+  if (!data || data.length === 0) notFound();
 
   return (
-    <div>
-      <h1 className="uppercase">Вопросы по {category}</h1>
-      <QuestionsList articles={articles} />
+    <div className={style.content}>
+      <h1 className={style.mainTitle}>
+        {isPortfolio ? "Мои проекты" : `Вопросы по ${category.toUpperCase()}`}
+      </h1>
+
+      {isPortfolio ? (
+        <div className={style.projectsGrid}>
+          {(data as IProject[]).map((project) => (
+            <ProjectCard key={project._id} project={project} />
+          ))}
+        </div>
+      ) : (
+        <QuestionsList articles={data as IArticle[]} />
+      )}
     </div>
   );
 }
--- src/lib/getArticles.ts
+++ src/lib/getArticles.ts
@@ -1,20 +1,25 @@
 // Получение данных из БД
-
 import dbConnect from "./dbConnect";
 import mongoose from "mongoose";
 
-export async function getArticles(category: string) {
+export async function getArticles<T>(
+  category: string,
+  collectionName: string = "articles",
+): Promise<T[]> {
   await dbConnect();
 
-  if (!mongoose.connection.db) {
-    throw new Error("База данных не подключена");
-  }
+  const db = mongoose.connection.db;
+  if (!db) throw new Error("База данных не найдена");
 
-  const collection = mongoose.connection.db.collection("articles");
-  const data = await collection.find({ category }).toArray();
+  const collection = db.collection(collectionName);
+
+  const query = collectionName === "projects" ? {} : { category };
+  // console.log(`Запрос к коллекции "${collectionName}" с фильтром:`, query);
+
+  const data = await collection.find(query).toArray();
 
   return data.map((item) => ({
     ...item,
     _id: item._id.toString(),
-  }));
+  })) as T[];
 }
